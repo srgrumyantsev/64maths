286f52f4ba5b7bceef3d44750795992cf66c86f1
srgrumyantsev <78035793+srgrumyantsev@users.noreply.github.com>
2022-06-21 23:56:09 +0700

    Delete quiz-rezult.js

--- js/quiz-rezult.js
+++
@@ -1,8 +0,0 @@
-function retrieveRecords(){ //retrieves items in the localStorage
-    var final-score = localStorage.getItem('ged-score'); //searches for the key in localStorage
-    var paragraph = document.createElement("p");
-    var infor = document.createTextNode(records);
-    paragraph.appendChild(infor);
-    var element = document.getElementById("retrieve");
-    element.appendChild(paragraph);
-}
